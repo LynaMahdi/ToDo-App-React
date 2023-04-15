6c806e9c55e4d726521626a2d9d397b0ae2b2bdd
unknown <kl_mahdi@esi.dz>
2023-04-15 19:01:00 +0100

    todo

--- src/components/todo.js
+++ src/components/todo.js
@@ -11,21 +11,31 @@ import './todo.css'
 
 
 const Todo=()=>{
+    const[i,setI]=useState(0)
     const[todo,setTodo]=useState("")
     const[list,setList]=useState([])
+    const[complist,setcompList]=useState([])
+    const[incomplist,setincompList]=useState([])
     const[light,setLight]=useState(false)
-    const[check,setCheck]=useState(true)
-    console.log(light)
-    
+    const[check,setCheck]=useState(false)
+    const tache={
+        id:  Math.floor((Math.random() * 100) + 1),
+        c: check,
+        value: todo
+
+    };
+
+    const total=[list,complist,incomplist]
     function Lida(){
 
         setLight(!light)
-        if(light===true){
-        document.querySelector("body").style.backgroundColor="white"
+        if(light===false){
+        document.querySelector("body").style.backgroundColor=" hsl(236, 33%, 92%)"
         document.getElementById("cnt").style.backgroundColor="white"
         document.getElementById("cnt1").style.backgroundColor="white"
         document.getElementById("cr").style.backgroundColor="white"
         document.getElementById("in").style.backgroundColor="white"
+        document.getElementById("in").style.color="black"
 
         }else{
 
@@ -34,46 +44,79 @@ const Todo=()=>{
         document.getElementById("cnt1").style.backgroundColor="hsl(237, 14%, 26%)"
         document.getElementById("cr").style.backgroundColor="hsl(237, 14%, 26%)"
         document.getElementById("in").style.backgroundColor="hsl(237, 14%, 26%)"
+        document.getElementById("in").style.color="hsl(234, 39%, 85%)"
 
         }
     }
     function Check(){
         setCheck(!check)
-        if(check===true){
-            document.getElementById("cr").style.backgroundImage="linear-gradient( hsl(192, 100%, 67%) , hsl(280, 87%, 65%))"
+        if(check===false){
             
+            document.getElementById("cr").style.backgroundImage="linear-gradient( hsl(192, 100%, 67%) , hsl(280, 87%, 65%))"
+
 
         }else{
+            if(light===false){
             document.getElementById("cr").style.backgroundImage="linear-gradient( hsl(237, 14%, 26%) , hsl(237, 14%, 26%))"
-            
+        }else{
+            document.getElementById("cr").style.backgroundImage="linear-gradient( white , white)"}
         }
     }
-    function AddTodo(){
-        const tache={
-            id:  Math.floor((Math.random() * 100) + 1),
-            c: check,
-            value: todo
 
-        };
+
+
+    function Check_todo(id){
+        let n_list=list.map((tache)=>(
+        {...tache,c: id===tache.id? !tache.c:tache.c}));
+        return setList(n_list);
+      }
+
+
+    function AddTodo(){
+        
         if(todo===""){
             alert("Créez d'abord une tache!")
            return;
         }
         setList(l=>[...l,tache])
         setTodo("")
-        console.log(list)
     }
+   
+    function completed_list(){
+        const comp=list.filter(l=>l.c===true)
+        setcompList(comp)
+        setI(1)
+        
+    }
+    function incompleted_list(){
+        const comp=list.filter(l=>l.c!==true)
+        setincompList(comp)
+        setI(2)
+    }
+   
+    
+    function delete_complete(){
+        const comp=list.filter(l=>l.c!==true)
+        setList(comp)
+
+    }
+    function delete_todo(id){
+        const nv_list=list.filter(l=>l.id!==id)
+        setList(nv_list)
+    }
+
+
     return(
     <>
        <div className='top'>
             {light? <img src= {lighti} alt="dark"/> :  <img src= {dark} alt="light"/>}
             <div>
                 <h>T O D O</h>
-                {light? <img className='sun' src={sun} alt="sun" onClick={Lida}/>:<img className='sun' src={moon} alt="moon" onClick={Lida}/>}
+                {light? <img className='sun' src={moon} alt="sun" onClick={Lida}/>:<img className='sun' src={sun} alt="moon" onClick={Lida}/>}
             </div>
 
             <div className='container' style={{top:`165px` , left:`495px`}} id='cnt' >
-                <div  id="cr" className="circleBase circle1" onClick={Check}>{check? "" :<img id="chichi" className='chichi' src={icoc} alt="icoc"/>}</div>
+                <div  id="cr" className="circleBase circle1" onClick={Check}>{check? <img id="chichi" className='chichi' src={icoc} alt="icoc"/>:""}</div>
                 <input id="in" type='text' placeholder='Create a new todo...' value={todo} onChange={e=>setTodo(e.target.value)} />
                 <FaArrowRight className='add' onClick={()=>AddTodo()}/>
             </div>
@@ -81,9 +124,30 @@ const Todo=()=>{
             
             <ul>
                 
-                {list.map(tache=>{return( <div id='cnt1' className='container' style={{top: `-260px`  , bottom: `0px`,left: `455px`}}><div  id="cr" className="circleBase circle1" onClick={Check}><img id="chichi" className='chich' src={cross} alt="cross"/>{tache.c? "" :<img id="chichi" className='chichi' src={icoc} alt="icoc"/>}</div><p key={tache.id}>{tache.value}</p></div>)})}
-                <div id='cnt1' className='container' style={{top: `-260px`  , bottom: `0px`,left: `455px`}} ><h1  style={{left: `20px`}}>{list.length} items left</h1><h1 style={{left: `220px`}}>All</h1><h1 style={{left: `252px`}}>Active</h1><h1 style={{left: `300px`}}>Completed</h1><h1 style={{left: `425px`}}>Clear Completed</h1></div>
+                {total[i].map(tache=>{return( 
+                 <div id='cnt2' className={light? 'container1':'container'}style={{top: `-260px`  , bottom: `0px`,left: `455px`}}>
+                    
+                    <div  id="cr" className="circleBase circle1" onClick={()=>Check_todo(tache.id)}>
+                        
+                      
+                        {tache.c?   <><div className="circleBase circleB"/><img  className='chichi' src={icoc} alt="icoc"/></>:""}
+                    </div>
+                    <img  className='chich' src={cross} alt="cross" onClick={()=>delete_todo(tache.id)}/>
+                    <p key={tache.id} className={light? "p2":"p1"}>{tache.value}</p>
+                 </div>)})}
+
+                <div id='cnt1' className='container' style={{top: `-260px`  , bottom: `0px`,left: `455px`}} ><h1  className={light? "h12":""}  style={{left: `20px`}}>{total[i].length} items left</h1><h1  className={light? "h12":""}  style={{left: `220px`}} onClick={()=>setI(0)}>All</h1><h1  className={light? "h12":""} style={{left: `252px`}} onClick={incompleted_list}>Active</h1><h1  className={light? "h12":""} style={{left: `300px`}} onClick={completed_list}>Completed</h1><h1  className={light? "h12":""} style={{left: `425px`}} onClick={delete_complete}>Clear Completed</h1></div>
+
             </ul>
+
+
+            <div class="wrapper">
+            <div>One</div>
+  <div>Two</div>
+  <div>Three</div>
+  <div>Four</div>
+  <div>Five</div>
+</div>
            
         </div>
     </>
